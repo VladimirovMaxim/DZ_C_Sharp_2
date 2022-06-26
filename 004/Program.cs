3430e11582a0b9c1db723574b1a812a4f784a172
maxim_vladimirov <maxvladimimrov@gmail.com>
2022-06-26 23:11:09 +0300

    DZ_finally

---
+++ 004/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13
+int ShowThirdDigit(int n )
+{
+    
+    if( n < 100)
+    {
+        n = 0;
+        return n ;
+    }
+    else
+    {
+        while(n  > 1000)
+        {
+            n = n / 10;
+        }
+        n = n % 10;  
+        return n ;
+    }
+    
+}
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int num = ShowThirdDigit(n );
+if( num == 0) 
+{
+    Console.WriteLine("У числа нет третьей цифры");
+}
+else
+{
+    Console.WriteLine( "Третья цифра " + num);
+}
---
+++ 015/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 15
+void WeekDay(int n)
+{
+    if (n>0 && n<8)
+    {
+
+        if ( n == 6 | n == 7)
+        { 
+            Console.WriteLine("ДА");
+        }
+        else
+        {
+            Console.WriteLine("НЕТ");
+        }
+        
+    }
+    else 
+    {
+        Console.WriteLine("Нет такого дня недели");
+    }
+}
+Console.Write("Введите день недели цифрой ");
+int n = Convert.ToInt32(Console.ReadLine());
+WeekDay(n);
